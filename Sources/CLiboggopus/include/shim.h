3334fe8f62e6d49346f066dbca16d56d0951fcc8
Angelo Paparazzi <Angelo.Paparazzi@ibm.com>
2021-01-19 15:52:25 -0500

    test

--- Sources/CLiboggopus/include/shim.h
+++ Sources/CLiboggopus/include/shim.h
@@ -4,10 +4,10 @@
 #ifndef shim_h
 #define shim_h
 
-#include <Headers/ogg.h>
 #include <Headers/opus.h>
-#include <Headers/config_types.h>
 #include <Headers/opus_header.h>
 #include <Headers/opus_multistream.h>
+#include <Headers/ogg.h>
+#include <Headers/config_types.h>
 
 #endif /* shim_h */
